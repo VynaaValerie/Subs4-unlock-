fb735d836f02c1cf1dc9f7e48371f958e372c41e
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2024-08-06 15:45:41 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {
+    mainTitle: "Unlock the URL",
+    mainText: "Click the buttons below to like the video and subscribe to the channel.",
+    likeUrl: "https://youtu.be/34c6mpZ4pK0?si=ehm-jTf_59vBpIBa",
+    subscribeUrl: "https://youtube.com/@vynaachan?si=_e--4ATV5xqYbcCd",
+    button1Text: "SimSimi no enc",
+    button1Url: "https://sfl.gl/X81HOeK9",
+    button2Text: "Base Fitur Games",
+    button2Url: "https://sfl.gl/4mln",
+    button3Text: "Dana Kaget",
+    button3Url: "https://link.dana.id/kaget?c=s4wmnkwve&r=hyFpX5",
+    slideshowImages: [
+        "https://telegra.ph/file/c617ebe0731d089e5f4d0.jpg",
+        "https://telegra.ph/file/748e77ecdb7f670dedc13.jpg"
+    ]
+};
