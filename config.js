2ed71daded838fc50cb90f84adcc2416725c617b
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2024-09-18 12:08:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
     mainTitle: "Unlock the URL",
     mainText: "Click the buttons below to like the video and subscribe to the channel.",
-    likeUrl: "https://youtu.be/34c6mpZ4pK0?si=ehm-jTf_59vBpIBa",
-    subscribeUrl: "https://youtube.com/@vynaachan?si=_e--4ATV5xqYbcCd",
-    button1Text: "SimSimi no enc",
-    button1Url: "https://sfl.gl/X81HOeK9",
-    button2Text: "Base Fitur Games",
-    button2Url: "https://sfl.gl/4mln",
-    button3Text: "Dana Kaget",
-    button3Url: "https://link.dana.id/kaget?c=s4wmnkwve&r=hyFpX5",
+    likeUrl: "https://youtu.be/ZwJ_lmAASXE?si=nB1LrtKAo2RZTPpJ",
+    subscribeUrl: "https://www.vynaachan-api.shop",
+    button1Text: "Gabut",
+    button1Url: "https://sfl.gl/VaoZ7b5",
+    button2Text: "Dana Kaget 1",
+    button2Url: "https://sfl.gl/TAfVqU4",
+    button3Text: "Dana Kaget 2",
+    button3Url: "https://sfl.gl/Vfb2ctc",
     slideshowImages: [
         "https://telegra.ph/file/c617ebe0731d089e5f4d0.jpg",
         "https://telegra.ph/file/748e77ecdb7f670dedc13.jpg"
